02911f9a9476428d0dae2045a2ac74051067855d
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-09-16 01:34:15 -0500

    SeguimientoDeCamaraCompleto

--- Assets/Scripts/AuxiliarDEBUG/SeguirMouse.cs
+++ Assets/Scripts/AuxiliarDEBUG/SeguirMouse.cs
@@ -7,10 +7,6 @@ public class SeguirMouse : MonoBehaviour
     private void Update()
     {
         //Actualizamos constantemente su posicion con el mouse
-        transform.position = new Vector2(
-            Mathf.Clamp(MouseLimiter.Instance.CoorMouse.x, -1.738f, 1.744f),
-            Mathf.Clamp(MouseLimiter.Instance.CoorMouse.y, -1.248f, 1.262f)
-            );
-            
+        transform.position = MouseLimiter.Instance.CoorMouse;
     }
 }
